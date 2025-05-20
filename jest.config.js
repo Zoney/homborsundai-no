19da559dbf7927e1a88613af065c60a671b5470c
Øyvind Brekkhus Sandåker <oyvind.sandaker@gmail.com>
2025-05-20 11:20:08 +0200

    Adjust jest setup file path

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+}
+
+module.exports = createJestConfig(customJestConfig)
---
+++ lib/utils.test.ts
@@ -0,0 +1,7 @@
+import { cn } from './utils'
+
+describe('cn', () => {
+  it('merges class names', () => {
+    expect(cn('foo', 'bar')).toBe('foo bar')
+  })
+})
